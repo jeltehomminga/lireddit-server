756e732fee8281916dee27abd7ffa72cfcae4057
Jelte Homminga <jeltehomminga@MacBook-Air-van-Jelte.local>
2020-09-14 09:32:00 +0200

    add stuff for sessions

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { MikroORM } from '@mikro-orm/core';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import 'reflect-metadata';
+import { buildSchema } from 'type-graphql';
 // import { Post } from "./entities/Post";
-import ormConfig from "./mikro-orm.config";
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import { buildSchema } from "type-graphql";
-import { HelloResolver } from "./resolvers/hello";
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
+import ormConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
+import { UserResolver } from './resolvers/user';
 
 const main = async () => {
   const orm = await MikroORM.init(ormConfig);
@@ -19,15 +18,13 @@ const main = async () => {
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
-      validate: false,
+      validate: false
     }),
-    context: () => ({ em: orm.em }),
+    context: () => ({ em: orm.em })
   });
 
   apolloServer.applyMiddleware({ app });
-  app.listen(4000, () => console.log("server started on 4000"));
+  app.listen(4000, () => console.log('server started on 4000'));
 };
 
-console.log("Hello who is this");
-
-main().catch((err) => console.log(err));
+main().catch(err => console.log(err));
